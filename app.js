efdb22c9ab90feb7b8e8021b4078546545e52f06
Lighton Phiri <lighton.phiri@gmail.com>
2019-12-28 20:00:13 +0200

    Added database connection
    Connected application to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+// dotenv
+// Use configuration entries in ".env" file
+require("dotenv").config();
+
+// Express JS
 const express = require("express");
 const app = express();
+
+// Mongoose
+const mongoose  = require("mongoose");
+mongoose.connect(
+    process.env.DATABASE_URL,
+    {
+        useNewUrlParser: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", (error) => {
+    console.log(`Database Error: ${error}`); // check to see if there are database errors
+});
+
+db.once("open", () => {
+    console.log(`Successfully connected to database: ${db.name} on ${db.host}`);
+});
 
 const indexRouter = require("./routes/index");
 const internsRouter = require("./routes/interns");
@@ -9,6 +32,11 @@ const projectsRouter = require("./routes/projects");
 app.use("/", indexRouter);
 app.use("/interns", internsRouter);
 app.use("/projects", projectsRouter);
+
+
+// Configure database connection
+
+
 
 // Configure port where application will run from
 const PORT = process.env.PORT || 9000;

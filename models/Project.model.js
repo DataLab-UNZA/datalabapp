d19933e4ff31c3d858537b0febf3ef2b961fdec1
Lighton Phiri <lighton.phiri@gmail.com>
2019-12-28 20:36:04 +0200

    Added database schema
    Added Projects schema
    Rendering projects documents as JSON

---
+++ models/Project.model.js
@@ -0,0 +1,27 @@
+/*
+> db.datalabProjects.find().pretty()
+{
+        "_id" : ObjectId("5e07861166263facea099631"),
+        "projectCode" : "Project 1",
+        "projectName" : "IR Deposit",
+        "projectDescription" : "MERN stack-based Web application for processing ETDs submitted by students at The University of Zambia",
+        "responsibility" : [
+                "Jackson Mwanaumo",
+                "Mathews Lungu",
+                "Wezi Munthali",
+                "Hockings Mambwe"
+        ]
+}
+*/
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ProjectSchema = new Schema({
+    projectCode: String,
+    projectName: String,
+    ProjectDescription: String,
+    responsibility: Array
+});
+
+module.exports = mongoose.model("Projects", ProjectSchema, "datalabProjects");
--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,22 @@
 const express = require("express");
 
 const router = express.Router();
+const Projects = require("../models/Project.model");
+
 
 router.get("/", (req, res) => {
-    res.send("/projects Router");
+    //res.send("/projects Router");
+    Projects.find({}).exec((error, projects) => {
+        if(error) {
+            // Check for errors
+            console.log(`Error extracting projects from database. Error: ${error}`);
+            res.send(`Error extracting projects from database.`);
+        } else {
+            // Render projects
+            console.log(`Extracting projects: ${projects}`);
+            res.json(projects);
+        }
+    });
 });
 
 module.exports = router;

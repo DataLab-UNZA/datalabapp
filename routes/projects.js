e810831e2b106a50f32e24c4909d7e772ce157c0
Lighton Phiri <lighton.phiri@gmail.com>
2019-12-28 21:27:49 +0200

    Project router CRUD implementation
    Added 'get' routers for extracting individual
    records

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const Projects = require("../models/Project.model");
 
-
+// Router to extract all projects in the database
 router.get("/", (req, res) => {
     //res.send("/projects Router");
     Projects.find({}).exec((error, projects) => {
@@ -14,6 +14,28 @@ router.get("/", (req, res) => {
         } else {
             // Render projects
             console.log(`Extracting projects: ${projects}`);
+            res.json(projects);
+        }
+    });
+});
+
+
+// Router to extract projects with name or description matching search text
+router.get("/search/project/:searchText", (req, res) => {
+    const searchText = req.params.searchText;
+    //res.send("/search/project/:searchText");
+    Projects.find({$or: [
+        {projectDescription: {$regex: '.*' + searchText + '*.'}}, 
+        {projectName: {$regex: '.*' + searchText + '*.'}},
+        {projectCode: {$regex: '.*' + searchText + '*.'}}
+    ]}).exec((error, projects) => {
+        if(error) {
+            //
+            console.log(`Error searching through projects.`);
+            res.send(`Error searching through projects`);
+        } else {
+            //
+            console.log(`Extracting projects with phrase: ${searchText}`);
             res.json(projects);
         }
     });

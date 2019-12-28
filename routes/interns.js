2dd02c0f35f11f32d756bcb331327430daff4f75
Lighton Phiri <lighton.phiri@gmail.com>
2019-12-28 21:06:13 +0200

    Intern router CRUD implementation
    Added 'get' routers for extracting individual
    records

--- routes/interns.js
+++ routes/interns.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const Intern = require("../models/Intern.model");
 
+// Router for extracting all records
 router.get("/", (req, res) => {
     //res.send("/authors router");
     Intern.find({}).exec((error, interns) => {
@@ -15,6 +16,42 @@ router.get("/", (req, res) => {
             //
             console.log(`Extracting Interns: ${interns}`);
             res.json(interns)
+        }
+    });
+});
+
+// Router for extracting a single record by studetID
+router.get("/search/id/:studentID", (req, res) => {
+    
+    // res.send("/search/:studetnID");
+    const searchKey = req.params.studentID;
+    // Experiment with wild character: studentID: {$regex: '*' + searchKey + '*'}
+    Intern.find({studentID: searchKey}).exec((error, student) => {
+        if(error) {
+            //
+            console.log(`Error extracting student with ID: ${searchKey}`);
+            res.send(`Error extracting student: ${searchKey}`);
+        } else {
+            //
+            console.log(`Found student: ${searchKey}`);
+            res.json(student);
+        }
+    });
+});
+
+// Router for extracting a single record by fullname
+router.get("/search/name/:fullName", (req, res) => {
+    //res.send("/search/name:fullName");
+    const fullName = req.params.fullName;
+    Intern.find({fullName: {$regex: '.*' + fullName + '.*'}}).exec((error, fullNames) => {
+        if(error) {
+            //
+            console.log(`Error extracting name from database. Error: ${error}`);
+            res.send(`Error extracting name from database.`);
+        } else {
+            //
+            console.log(`Extracted records matching fullName: ${fullNames}`);
+            res.json(fullNames);
         }
     });
 });

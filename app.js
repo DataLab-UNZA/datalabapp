b1dcaf5474cdad7ca7a4314d4060630ef71f4d6a
Lighton Phiri <lighton.phiri@gmail.com>
2019-12-28 19:21:02 +0200

    Added routers
    Added index, interns and projects routers

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const indexRouter = require("./routes/index");
+const internsRouter = require("./routes/interns");
+const projectsRouter = require("./routes/projects");
+
+// Configure sub routers
+app.use("/", indexRouter);
+app.use("/interns", internsRouter);
+app.use("/projects", projectsRouter);
+
 // Configure port where application will run from
 const PORT = process.env.PORT || 9000;
 

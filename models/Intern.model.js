8e516d5cd6d693aadf5b5b6ad2679ce606761b01
Lighton Phiri <lighton.phiri@gmail.com>
2019-12-28 20:27:15 +0200

    Added database schema
    Added Interns schema
    Rendering interns documents as JSON

---
+++ models/Intern.model.js
@@ -0,0 +1,34 @@
+/*
+> db.datalabRAs.find().limit(1).pretty()
+{
+        "_id" : ObjectId("5e07828266263facea099549"),
+        "fullName" : "Mathews Lungu",
+        "studentID" : "2017012950",
+        "emailAddress" : [
+                "2017012950@student.unza.zm",
+                "muyaz068@gmail.com"
+        ],
+        "phoneNumber" : [
+                "+260963137684"
+        ],
+        "residentialAddress" : "Garden Park"
+}
+*/
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const InternSchema = new Schema({
+    // Notice the different ways of defining a schema entry
+    fullName: {
+        type: String,
+        required: true
+    },
+    studentID: String,
+    emailAddress: Array,
+    phoneNumber: Array,
+    residentialAddress: String
+});
+
+
+module.exports = mongoose.model("Intern", InternSchema, "datalabRAs");
--- routes/interns.js
+++ routes/interns.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+const Intern = require("../models/Intern.model");
+
 router.get("/", (req, res) => {
-    res.send("/authors router");
+    //res.send("/authors router");
+    Intern.find({}).exec((error, interns) => {
+        if(error) {
+            //
+            console.log(`Error extracting Interns from the database. Error: ${error}`);
+            res.send(`Error extracting Interns from database.`);
+        } else{
+            //
+            console.log(`Extracting Interns: ${interns}`);
+            res.json(interns)
+        }
+    });
 });
 
 module.exports = router;
